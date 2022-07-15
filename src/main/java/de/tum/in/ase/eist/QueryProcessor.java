55aaead8c97e8124a11ea8595c2191c927eb98d8
SHAO Wanqiu <shaowanqiu66@gmail.com>
2022-07-15 12:53:31 +0200

    BasicWebApp

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,7 +12,7 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "Shao Wanqiu";
+           return "ShaoWanqiu";
         } else { // TODO extend the programm here
             return "";
         }
